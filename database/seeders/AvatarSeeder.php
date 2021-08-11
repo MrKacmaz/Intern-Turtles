2ea8d5b9f06d9035c828deb058054323b5d178dd
MrKacmaz <alperen703.akm@gmail.com>
2021-08-11 18:04:03 +0300

    test

--- database/seeders/AvatarSeeder.php
+++ database/seeders/AvatarSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class AvatarSeeder extends Seeder
 {
@@ -13,6 +14,12 @@ class AvatarSeeder extends Seeder
      */
     public function run()
     {
-        //
+
+        DB::table('avatars')->insert([
+            'npcName' => 'Micmicello',
+            'npcImagePath' => 'asdasdasd',
+            'npcBase' => 2,
+            'npcLevel' => 1,
+        ]);
     }
 }
