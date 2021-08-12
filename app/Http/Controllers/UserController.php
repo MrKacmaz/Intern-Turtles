9af3f34e2c12b7375e8b8828dd6e86b7b6db059a
HandeYeltekin <87368350+HandeYeltekin@users.noreply.github.com>
2021-08-12 17:37:04 +0300

    login blade updated

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\userLevel  $userLevel
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\userLevel  $userLevel
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(User $userLevel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\userLevel  $userLevel
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $userLevel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\userLevel  $userLevel
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $userLevel)
+    {
+        //
+    }
+}
