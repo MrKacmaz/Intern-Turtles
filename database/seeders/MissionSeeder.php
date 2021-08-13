51a02fb8a906f8db440c46c3ff08ab9f65f92f0c
MrKacmaz <alperen703.akm@gmail.com>
2021-08-13 14:12:26 +0300

    mission seeder finito

--- database/seeders/MissionSeeder.php
+++ database/seeders/MissionSeeder.php
@@ -14,14 +14,110 @@ class MissionSeeder extends Seeder
      */
     public function run()
     {
-        //
+
         DB::table('missions')->insert([
-            'missionLevel'=>' ',//str
-            'missionName'=>' ',//str
-            'missionStatus'=>' ',//str
-            'missionLocation'=>' ',//FK
-            'missionMoney'=>' ',//int
-            'missionExp'=>' ',//int
+            'missionLevel' => 1, //int
+            'missionName' => 'Kalacağın yeri pırıl pırıl yap', //str
+            'missionStatus' => 'Main', //str
+            'missionLocation' => 1, //FK
+            'missionMoney' => 25, //int
+            'missionExp' => 25, //int
+        ]);
+
+        DB::table('missions')->insert([
+            'missionLevel' => 1, //int
+            'missionName' => 'Karanlık ormanı aç', //str
+            'missionStatus' => 'Main', //str
+            'missionLocation' => 2, //FK
+            'missionMoney' => 100, //int
+            'missionExp' => 50, //int
+        ]);
+
+        DB::table('missions')->insert([
+            'missionLevel' => 1, //int
+            'missionName' => 'Domin Usta`ya uğra ve tanış', //str
+            'missionStatus' => 'Main', //str
+            'missionLocation' => 7, //FK
+            'missionMoney' => 100, //int
+            'missionExp' => 75, //int
+            // Ekstra olarak 2 pizza kazanacak
+        ]);
+
+        DB::table('missions')->insert([
+            'missionLevel' => 1, //int
+            'missionName' => 'Demirciyi aç', //str
+            'missionStatus' => 'Main', //str
+            'missionLocation' => 9, //FK
+            'missionMoney' => 100, //int
+            'missionExp' => 100, //int
+            // Damage power + 10
+        ]);
+
+        DB::table('missions')->insert([
+            'missionLevel' => 2, //int
+            'missionName' => 'Lağımı aç', //str
+            'missionStatus' => 'Main', //str
+            'missionLocation' => 5, //FK
+            'missionMoney' => 100, //int
+            'missionExp' => 150, //int
+        ]);
+
+        DB::table('missions')->insert([
+            'missionLevel' => 2, //int
+            'missionName' => 'Bomba imha araştırmasını tamamla', //str
+            'missionStatus' => 'Main', //str
+            'missionLocation' => 5, //FK
+            'missionMoney' => 200, //int
+            'missionExp' => 300, //int
+            // ekstra 1 pizza
+        ]);
+
+        DB::table('missions')->insert([
+            'missionLevel' => 2, //int
+            'missionName' => 'Bilgileri ulaştır', //str
+            'missionStatus' => 'Main', //str
+            'missionLocation' => 1, //FK
+            'missionMoney' => 200, //int
+            'missionExp' => 500, //int
+        ]);
+
+        DB::table('missions')->insert([
+            'missionLevel' => 3, //int
+            'missionName' => 'Demirciyi uyar', //str
+            'missionStatus' => 'Main', //str
+            'missionLocation' => 9, //FK
+            'missionMoney' => 100, //int
+            'missionExp' => 300, //int
+
+            // demirci yan görevleri aktif edilecek
+        ]);
+
+        DB::table('missions')->insert([
+            'missionLevel' => 3, //int
+            'missionName' => 'Pizzacıda son keyifleri yap', //str
+            'missionStatus' => 'Main', //str
+            'missionLocation' => 7, //FK
+            'missionMoney' => 0, //int
+            'missionExp' => 0, //int
+        ]);
+
+        DB::table('missions')->insert([
+            'missionLevel' => 3, //int
+            'missionName' => 'Kötü adamları alt et', //str
+            'missionStatus' => 'Main', //str
+            'missionLocation' => 5, //FK
+            'missionMoney' => 500, //int
+            'missionExp' => 600, //int
+            // ekstra 2 pizza
+        ]);
+
+        DB::table('missions')->insert([
+            'missionLevel' => 3, //int
+            'missionName' => 'İyi bir dünya için son adım', //str
+            'missionStatus' => 'Main', //str
+            'missionLocation' => 5, //FK
+            'missionMoney' => 1000, //int
+            'missionExp' => 1000, //int
         ]);
     }
 }
