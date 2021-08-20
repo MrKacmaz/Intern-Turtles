a9abefe0b7982963cc5a70207f388629e27b2ea7
MrKacmaz <alperen703.akm@gmail.com>
2021-08-20 14:20:00 +0300

    blade updated

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -13,92 +13,68 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
 
-</head>
-
-<body>
-
-
-    <!-- W3 School -->
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
-
     <!-- Sweet alert -->
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <link rel="stylesheet" href="{{ asset('/css/fonts/font.css') }}">
     <link rel="stylesheet" href="{{ asset('/css/welcomeBlade.css') }}">
     <link rel="stylesheet" href="{{ asset('/css/fonts/font.css') }}">
 
+</head>
 
-    <button class="card w3-hover-light-grey btn" style="width: 18rem;"><img src="{{ $map->mapImagePath }}"
-            class="card-img-top">
-        <div class="card-body">
-            <h5 class="card-title text-center">{{ $map->mapName }}</h5>
-        </div>
-    </button>
+<body>
 
-
-
-    </head>
-
-    <body>
-
-        <div class="div">
-            <div class="row" style="text-align: center">
-
-                @foreach ($allMaps as $map)
-                    <form action="" method="post">
-                        @csrf {{ csrf_field() }}
-                        <img src="{{ $map->mapImagePath }}" class="card-img-top">
-                    </form>
-                @endforeach
+    <div class="div">
+        <div class="row" style="text-align: center">
+            @foreach ($allMaps as $map)
+                <form action="" method="post">
+                    @csrf {{ csrf_field() }}
+                    <img src="{{ $map->mapImagePath }}" class="card-img-top">
+                </form>
+            @endforeach
 
 
 
-                <button id="next" onclick="nextDialog()">Next</button>
-                <button id="reader" style="display: none">Reader</button>
+            <button id="next" onclick="nextDialog()">Next</button>
+            <button id="reader" style="display: none">Reader</button>
 
 
-                {{-- DIALOG --}}
-                @foreach ($baseDialog as $dialog)
-                    <div id="dialogDiv-{{ $dialog->id }}" style="display: none">
+            {{-- DIALOG --}}
+            @foreach ($baseDialog as $dialog)
+                <div id="dialogDiv-{{ $dialog->id }}" style="display: none">
 
-                        <p id="npcNameP-{{ $dialog->id }}">
-                            <b>{{ $dialog->npcName }}</b>
-                        </p>
+                    <p id="npcNameP-{{ $dialog->id }}">
+                        <b>{{ $dialog->npcName }}</b>
+                    </p>
 
-                        <p id=" textP-{{ $dialog->id }}">
-                            {{ $dialog->text }}
-                        </p>
-                    </div>
-                @endforeach
+                    <p id=" textP-{{ $dialog->id }}">
+                        {{ $dialog->text }}
+                    </p>
+                </div>
+            @endforeach
 
-
-            </div>
 
         </div>
 
+    </div>
 
-        <script>
-            var counter = 13;
-            $("#dialogDiv-12").show();
 
-            function nextDialog() {
+    <script>
+        var counter = 13;
+        $("#dialogDiv-12").show();
 
-                if (counter <= 24) {
-                    $("#dialogDiv-" + counter).show();
-                    $("#dialogDiv-" + (counter - 1)).hide();
-                    counter++;
-                }
-                if (counter == 25) {
-                    $("#next").show();
-                    $("#reader").hide();
-                }
+        function nextDialog() {
+            if (counter <= 24) {
+                $("#dialogDiv-" + counter).show();
+                $("#dialogDiv-" + (counter - 1)).hide();
+                counter++;
             }
-        </script>
+            if (counter == 25) {
+                $("#next").hide();
+                $("#reader").show();
+            }
+        }
+    </script>
 
-    </body>
+</body>
 
 </html>
