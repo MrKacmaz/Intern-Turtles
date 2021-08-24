4757d00cbf29e95a405da6b0b25c3437e4fb3a77
mdalkilic121 <cryanqel@gmail.com>
2021-08-24 17:27:59 +0300

    asd

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,4 +1,4 @@
-<div id="mySidenav" class="sidenav">
+<div id="mySidenav" class="sidenav" style="z-index: 101;">
     <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
     <a href="">Load Game</a>
     <a href="">New Game</a>
