21a4ca59df6590fbbd9c92367a59b8c43c812ef2
BerraOz <berraoz20@gmail.com>
2021-08-26 10:56:25 +0300

    forest blade update

--- resources/views/forest.blade.php
+++ resources/views/forest.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,13 +5,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Base</title>
+    <title>Dark Forest</title>
+
     <!-- W3 School -->
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+
+    <!-- JQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
     <!-- Bootstrap 5 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
@@ -29,7 +31,6 @@
     <!-- Sweet alert -->
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <link rel="stylesheet" href="{{ asset('/css/fonts/font.css') }}">
-
     <style>
         body {
             background-color: black;
@@ -113,11 +114,9 @@
         }
 
     </style>
-
 </head>
 
 <body>
-
     <div class="container">
 
         <div>
@@ -125,65 +124,64 @@
                 style="text-align-last:center; position:absolute; z-index:100; font-size:30px;cursor:pointer; color:blanchedalmond"
                 onclick="openNav()">&#9776;</span>
         </div>
-        @extends('layouts.navbar');
+       
 
         <div class="row" style="text-align: center">
 
             <div>
-                @foreach ($allMaps as $map)
+                @foreach ($forestMap as $i)
                     <form action="" method="post">
                         @csrf {{ csrf_field() }}
-                        <img class="map" src="{{ $map->mapImagePath }}" class="card-img-top">
+                        <img class="map" src="{{$i->mapImagePath }}" class="card-img-top">
                     </form>
                 @endforeach
             </div>
 
             <div class="dialog">
                 {{-- DIALOG --}}
-                @foreach ($baseDialog as $dialog)
-                    <div id="dialogDiv-{{ $dialog->id }}"
+                @foreach ($forestDialogs as $j)
+                    <div id="dialogDiv-{{  $j->id }}"
                         style="display: none; text-align: center; background: rgba(0, 0, 0, 0.5); color: white;">
 
-                        <p id="npcNameP-{{ $dialog->id }}">
-                            {{ $dialog->npcName }}
+                        <p id="npcNameP-{{$j->id }}">
+                            {{ $j->npcName }}
                         </p>
 
-                        <p class="dialog-body" id=" textP-{{ $dialog->id }}">
-                            {{ $dialog->text }}
+                        <p class="dialog-body" id=" textP-{{$j->id}}">
+                            {{$j->text }}
                         </p>
                     </div>
                 @endforeach
 
                 <div class="buttons">
-                    <a id="next" style="display: none; cursor: pointer;" href="{{ url('/base/2') }}"><svg
-                            xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor"
+                    <a href="{{ url('/maps') }}" id="next" style="display: none; cursor: pointer;" href="#"><svg
+                            xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" style="color: antiquewhite"
                             class="bi bi-chevron-double-right" viewBox="0 0 16 16">
                             <path fill-rule="evenodd"
                                 d="M3.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L9.293 8 3.646 2.354a.5.5 0 0 1 0-.708z" />
                             <path fill-rule="evenodd"
                                 d="M7.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L13.293 8 7.646 2.354a.5.5 0 0 1 0-.708z" />
                         </svg></a>
-
-                    <a id="nextMission" style="display: none; cursor: pointer;" href="{{ url('/maps') }}"><svg
-                            xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor"
-                            class="bi bi-chevron-double-right" viewBox="0 0 16 16">
+            
+                    <a id="nextMission" style="display: none; cursor: pointer;" href="#"><svg xmlns="http://www.w3.org/2000/svg"
+                            width="32" height="32" fill="currentColor" style="color: antiquewhite" class="bi bi-chevron-double-right" viewBox="0 0 16 16">
                             <path fill-rule="evenodd"
                                 d="M3.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L9.293 8 3.646 2.354a.5.5 0 0 1 0-.708z" />
                             <path fill-rule="evenodd"
                                 d="M7.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L13.293 8 7.646 2.354a.5.5 0 0 1 0-.708z" />
                         </svg></a>
-
+            
                     <a id="reader" onclick="nextDialog({{ $userNpc }})" style="cursor: pointer"><svg
-                            xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor"
+                            xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" style="color: antiquewhite"
                             class="bi bi-chevron-right" viewBox="0 0 16 16">
                             <path fill-rule="evenodd"
                                 d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z" />
                         </svg></a>
                 </div>
+            
             </div>
         </div>
     </div>
-
     <div class="progress">
         <div id="bar" class="progress-bar progress-bar-striped bg-danger" role="progressbar" style="width: 0%"
             aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
@@ -193,7 +191,9 @@
     <button id="btnIncrease" style="display: none" class="btn btn-outline-success"
         onclick="increaseFun()">Increase</button>
 
+   
     <script>
+
         function openNav() {
             document.getElementById("mySidenav").style.width = "250px";
         }
@@ -232,44 +232,25 @@
 
         }
 
-        var counter = 13;
-        var counter2 = 20;
-        $("#dialogDiv-12").show();
-        $("#dialogDiv-19").show();
+        var counter = 87;
+        $('#dialogDiv-86').show();
 
         function nextDialog(userNpc) {
-            if (window.location.pathname.split('')[6] == 1) {
-                if (counter <= 18) {
-                    $("#dialogDiv-" + counter).show();
-                    $("#dialogDiv-" + (counter - 1)).hide();
-                    counter++;
-                    npcNames(counter, userNpc);
-                }
-                if (counter == 19) {
-                    $("#reader").hide();
-                    $("#next").show();
-                }
+            if (counter <= 95) {
+                $("#dialogDiv-" + counter).show();
+                $("#dialogDiv-" + (counter - 1)).hide();
+                counter++;
+                npcNames(counter, userNpc);
             }
-            if (window.location.pathname.split('')[6] == 2) {
-                npcNames(counter2, userNpc);
-
-                if (counter2 <= 24) {
-                    $("#dialogDiv-" + counter2).show();
-                    $("#dialogDiv-" + (counter2 - 1)).hide();
-                    counter2++;
-                    npcNames(counter2, userNpc);
-
-                }
-                if (counter2 == 25) {
-                    $("#reader").hide();
-                    $("#nextMission").show();
-                }
+            if (counter == 96) {
+                $("#reader").hide();
+                $("#next").show();
             }
         }
 
         function npcNames(count, userNpc) {
-
-            switch ($("#npcNameP-" + (count - 1)).text().split('')[32]) {
+            console.log($("#npcNameP-" + (count - 1)).text().split(''));
+            switch ($("#npcNameP-" + (count - 1)).text().split('')[3]) {
                 case '1':
                     (userNpc == 1) ? $("#npcNameP-" + (count - 1)).text('Micmicello'):
                         (userNpc == 2) ? $("#npcNameP-" + (count - 1)).text('Lovabardo') :
@@ -299,7 +280,7 @@
                     break;
 
                 default:
-                    console.log("def e düştü");
+                    console.error("DEFAULT HAS BEEN WORKED");
                     break;
             }
         }
